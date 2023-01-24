8c6aa819a327b132a738111e87313a707be92bb6
PCCLONBCN\Alumno <aa>
2023-01-24 18:16:01 +0100

    Full Props && Order (Cev)

---
+++ Assets/Game/Scripts/Scripted Animations/Scr_Ani_Levitation.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_Ani_Levitation : MonoBehaviour
+{
+    
+    public float Amount;
+    public float Speed;
+
+    public float SpeedRotation;
+
+    Rigidbody RB;
+
+    float rotation;
+
+    float Timer;
+    float DefaultY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DefaultY = transform.localPosition.y;
+
+        RB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(RB.isKinematic)
+        {
+            Timer += Time.deltaTime * Amount;
+
+            transform.localPosition = new Vector3(
+            transform.localPosition.x, DefaultY + Mathf.Sin(Timer) * Speed,
+            transform.localPosition.z);
+
+            rotation += SpeedRotation * Time.deltaTime;
+
+            transform.rotation = Quaternion.Euler(0, rotation, 0);
+        }
+        
+    }
+}
