09e92e496c01541bf4e8486f13492bc1a998a6ea
Als <alexlorente01@gmail.com>
2023-03-15 20:11:55 +0100

    Demon Debug (House)

--- Assets/Scr_demon_Scream_High.cs
+++ Assets/Scr_demon_Scream_High.cs
@@ -11,6 +11,7 @@ public class Scr_demon_Scream_High : MonoBehaviour
     public Scr_Cordura Cordura;
 
     public GameObject Parent;
+    GameObject PlayerCam;
 
     public bool jump;
     public float TimeScream;
@@ -18,20 +19,22 @@ public class Scr_demon_Scream_High : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
-
+        PlayerCam = GameObject.Find("PlayerCamera");
     }
 
     private void Update()
     {
+        
+
         if(TimeScream > 5f && !jump)
         {
             jump = true;
-
+            Parent.transform.LookAt(PlayerCam.transform.position, Vector3.up);
 
             Animatorr.SetBool("Jump", true);
-            GameObject PlayerCam = GameObject.Find("PlayerCamera");
+            
             float Distance = Vector3.Distance(Parent.transform.position, PlayerCam.transform.position);
-            Parent.transform.DOMove(PlayerCam.transform.position + (Vector3.down*1f) + (Vector3.left*1.7f), Distance * 0.05f).SetEase(Ease.InSine);
+            Parent.transform.DOMove(PlayerCam.transform.position + (Vector3.down * 0.8f) + (Parent.transform.forward * -1 * 1.5f), Distance * 0.05f).SetEase(Ease.InSine);
         }
     }
 
